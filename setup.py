70fad0a3e203bae25ab7404bd64da0c19bea71b3
Michael Kerry <mkerry@fas.harvard.edu>
2021-03-03 14:40:44 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
 import setuptools
 
-setuptools.setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aais-flask-db-util",
+    version="0.0.1",
+    author="Jinesh Mehta",
+    author_email="jinesh_mehta@harvard.edu",
+    description="A utility for handling Oracle db operations within a Flask application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.huit.harvard.edu/HUIT/flask_db_util",
+    project_urls={
+        "Bug Tracker" : "https://github.huit.harvard.edu/HUIT/flask_db_util/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
